23ca1516f2dd2eb5fc5c5edf356524ec337ba307
Pablo Prieto <pablotsdaw@gmail.com>
2018-01-13 19:07:57 +0100

    StickyNotes Angular

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuSuperiorComponent } from './menu-superior/menu-superior.component';
 import { FormsModule } from '@angular/forms';
+import { NotaComponent } from './nota/nota.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuSuperiorComponent
+    MenuSuperiorComponent,
+    NotaComponent
   ],
   imports: [
     BrowserModule,
